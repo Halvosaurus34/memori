cd78b3c3deb2bb5c07130a46c80ab70863604270
Levi <lhalvorson34@gmail.com>
2021-06-11 13:07:24 -0600

    refactor

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,12 +1,10 @@
-import { FETCH_ALL, CREATE, UPDATE, DELETE } from "../constants/actionTypes";
-
-import * as api from "../api";
-
-//Action creators
+import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE } from '../constants/actionTypes';
+import * as api from '../api/index.js';
 
 export const getPosts = () => async (dispatch) => {
   try {
     const { data } = await api.fetchPosts();
+
     dispatch({ type: FETCH_ALL, payload: data });
   } catch (error) {
     console.log(error);
@@ -16,6 +14,7 @@ export const getPosts = () => async (dispatch) => {
 export const createPost = (post) => async (dispatch) => {
   try {
     const { data } = await api.createPost(post);
+
     dispatch({ type: CREATE, payload: data });
   } catch (error) {
     console.log(error);
@@ -25,7 +24,20 @@ export const createPost = (post) => async (dispatch) => {
 export const updatePost = (id, post) => async (dispatch) => {
   try {
     const { data } = await api.updatePost(id, post);
+
     dispatch({ type: UPDATE, payload: data });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const likePost = (id) => async (dispatch) => {
+  const user = JSON.parse(localStorage.getItem('profile'));
+
+  try {
+    const { data } = await api.likePost(id, user?.token);
+
+    dispatch({ type: LIKE, payload: data });
   } catch (error) {
     console.log(error);
   }
@@ -33,17 +45,9 @@ export const updatePost = (id, post) => async (dispatch) => {
 
 export const deletePost = (id) => async (dispatch) => {
   try {
-    await api.deletePost(id);
-    dispatch({ type: DELETE, payload: id });
-  } catch (error) {
-    console.log(error);
-  }
-};
+    await await api.deletePost(id);
 
-export const likePost = (id) => async (dispatch) => {
-  try {
-    const { data } = await api.likePost(id);
-    dispatch({ type: UPDATE, payload: data });
+    dispatch({ type: DELETE, payload: id });
   } catch (error) {
     console.log(error);
   }
